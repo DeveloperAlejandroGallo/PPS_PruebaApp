60e000db8f5fb1e01a80d8351eb673c588f216eb
Alejandro Gallo <alegallo@gmail.com>
2023-07-15 01:07:33 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PPS_PruebaApp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
