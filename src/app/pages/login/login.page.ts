d16ce1038de658cc64f56f3c21fc49d1a0b6b20d
Alejandro Gallo <alegallo@gmail.com>
2023-07-19 00:05:56 -0300

    Primer commit

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+login() {
+throw new Error('Method not implemented.');
+}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
